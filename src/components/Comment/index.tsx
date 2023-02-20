e2faf3252bcb205196febddbb35adf88c31c475d
Harsh Raj <harshraj8843@gmail.com>
2023-02-20 19:52:11 +0530

    format code

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,21 +1,21 @@
-'use client';
+"use client";
 
-import Giscus from '@giscus/react';
-import { useTheme } from 'next-themes';
+import Giscus from "@giscus/react";
+import { useTheme } from "next-themes";
 
 export default function Comment() {
   const { theme } = useTheme();
   return (
     <>
       <Giscus
-        id='comments'
+        id="comments"
         repo={`codinasion/comments`}
         repoId={`R_kgDOHb-6Gg`}
         category={`General`}
         categoryId={`DIC_kwDOHb-6Gs4CPbX4`}
         mapping={`url`}
         strict={`0`}
-        term='Welcome to Codinasion :)'
+        term="Welcome to Codinasion :)"
         reactionsEnabled={`1`}
         emitMetadata={`0`}
         inputPosition={`bottom`}
