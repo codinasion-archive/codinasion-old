0c04338dbaa9ca988cd05d7502ab640d0d1d0295
jaw162 <jwhite162@protonmail.com>
2023-02-21 15:25:41 +0000

    Added breadcrumb links to program page

--- src/app/program/[slug]/page.tsx
+++ src/app/program/[slug]/page.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import MarkdownPreview from "@/components/MarkdownPreview";
 import Comment from "@/components/Comment";
 import { AvailableSolutionCard, ContributorsCard } from "@/components/Program";
+import { useRouter } from "next/router";
 
 async function getProgramData(slug: string) {
   const res = await fetch(
@@ -24,6 +27,8 @@ export default async function ProgramPage({
 }) {
   const { slug } = params;
 
+  const router = useRouter();
+
   const ProgramData = await getProgramData(slug);
 
   return (
@@ -31,6 +36,17 @@ export default async function ProgramPage({
       <section className="py-8 px-4 lg:py-16 lg:px-6">
         <div className="grid grid-cols-1 md:grid-cols-6 gap-4 md:gap-8">
           <div className="md:col-span-4">
+            <aside>
+              <ul className="flex justify-start">
+                <li onClick={() => router.push("/")}>Home</li>
+                <li className="px-2">/</li>
+                <li onClick={() => router.push("/program")} className="px-2">
+                  Programs
+                </li>
+                <li className="px-2">/</li>
+                <li className="px-2">{ProgramData.title}</li>
+              </ul>
+            </aside>
             <MarkdownPreview>{ProgramData.markdown}</MarkdownPreview>
             <div className="my-10">
               <Comment />
