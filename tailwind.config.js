2c842b2458a091c26981c1f87ed161bd3ba873f6
Petrosdevri <petrosdevri@protonmail.com>
2023-02-21 16:40:20 +0200

    Added initial hover animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        scale: {
+          '0%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.2)' },
+          '100%': { transform: 'scale(1.5)' },
+        },
+      },
+      animation: {
+        scale: 'scale 1s ease-in-out',
+      }
+    },
   },
   plugins: [],
 };
