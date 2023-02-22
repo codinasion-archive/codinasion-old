d73aaf323ee7130a719c7ab39098c76954b27736
Harsh Raj <harshraj8843@gmail.com>
2023-02-22 18:41:52 +0530

    add breadcrump

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,21 @@
+import { GithubUserType, GithubUserArchiveType } from "./Archive";
+import { BreadcrumbType } from "./Breadcrump";
+import { ContributorType } from "./Contributors";
 import { FooterLinkType, FooterLinksType } from "./Footer";
 import { NavbarLinkType, NavbarLinksType } from "./Navbar";
-import { ProjectType } from "./Project";
 import { ProgramType, TagType } from "./Program";
+import { ProjectType } from "./Project";
 
 export type {
+  GithubUserType,
+  GithubUserArchiveType,
+  BreadcrumbType,
+  ContributorType,
   FooterLinkType,
   FooterLinksType,
   NavbarLinkType,
   NavbarLinksType,
-  ProjectType,
   ProgramType,
   TagType,
+  ProjectType,
 };
