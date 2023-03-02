4742b6dfa1eec299b351c94a040caecdb8088929
Harsh Raj <harshraj8843@gmail.com>
2023-03-02 14:29:51 +0530

    update repo card

--- src/components/RepoCard/index.tsx
+++ src/components/RepoCard/index.tsx
@@ -10,7 +10,7 @@ export default function RepoCard({ repo }: RepoCardProps) {
     <>
       <Link href={`https://github.com/${repo}`} externalIcon={false}>
         <Image
-          src={`https://gh-card.dev/repos/${repo}.svg`}
+          src={`https://gh-card.dev/repos/${repo}.svg?fullname=`}
           alt={`Repo Card for ${repo}`}
           className="w-full rounded-lg shadow-lg hover:shadow-xl my-5"
         />
