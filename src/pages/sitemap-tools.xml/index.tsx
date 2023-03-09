94cb7c4cc19094bef57925b4a0b5a968764d066e
Harsh Raj <harshraj8843@gmail.com>
2023-03-09 22:56:51 +0530

    add tools sitemap

---
+++ src/pages/sitemap-tools.xml/index.tsx
@@ -0,0 +1,24 @@
+import { getServerSideSitemapIndex } from "next-sitemap";
+import { GetServerSideProps } from "next";
+import SiteMetaData from "@/data/SiteMetaData";
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const sitemapData: string[] = [];
+  const allToolsData = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/tools/`,
+    {
+      method: "GET",
+    }
+  )
+    .then((res: any) => res.json())
+    .catch((error) => console.log(error));
+
+    allToolsData.map((tool: any) => {
+    sitemapData.push(`${SiteMetaData.site_url}/tools/${tool.slug}`);
+  });
+
+  return getServerSideSitemapIndex(ctx, sitemapData);
+};
+
+// Default export to prevent next.js errors
+export default function ToolsSitemap() {}
